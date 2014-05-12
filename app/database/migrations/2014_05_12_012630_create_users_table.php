d21e386d9d0ccc0725a90cac1599b8b83aeeb6ee
uvu <setkyar16@gmail.com>
2014-05-12 08:16:04 +0630

    Create User Table via migration

---
+++ app/database/migrations/2014_05_12_012630_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('password');
+			$table->string('password_temp');
+			$table->string('email');
+			$table->string('code');
+			$table->integer('active');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
