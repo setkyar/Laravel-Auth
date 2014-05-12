38948902eed1878983546ace546e16afcf281501
uvu <setkyar16@gmail.com>
2014-05-12 08:17:18 +0630

    Route for account/create

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,34 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('/', array(
+	'as' => 'home',
+	'uses' => 'HomeController@home'
+));
+
+/*
+	Unathenticated group
+*/
+Route::group(array('before' => 'guest'), function(){
+	/*
+		CSRF protection group
+	*/
+	Route::group(array('before' => 'csrf'), function() {
+		/*
+			Create account (POST)
+		*/
+		Route::post('/account/create', array(
+			'as' => 'account-create-post',
+			'uses' => 'AccountController@postCreate'
+		));
+	});
+
+	/*
+		Create account (GET)
+	*/
+	Route::get('/account/create', array(
+		'as' => 'account-create',
+		'uses' => 'AccountController@getCreate'
+	));
+
 });
